6ba29815903d61110148628da0806711e78a60a6
SmallMonkey-ai <2641973190@qq.com>
2022-06-06 16:49:10 +0800

    增加mock  方法并模拟真实数据返回

--- languageMock/airbnb-ssr/src/utils/indexedDB.ts
+++ languageMock/airbnb-ssr/src/utils/indexedDB.ts
@@ -1,3 +1,6 @@
+import { rejects } from "assert"
+import { resolve } from "path"
+
 class DB {
     private dbName: string
     private db: any //数据库对象
@@ -8,74 +11,91 @@ class DB {
     public openStore(storeName: string, keyPath: string, indexs?: Array<string>) {
         // 这里2表示数据库版本号 且版本号必须递增 默认为0
         const request = window.indexedDB.open(this.dbName, 1)
-        // 初始化时 顺序
-        request.onupgradeneeded = (event: any) => {
-            console.log('数据库升级成功', event)
-            const { result }: any = event.target
-            const store = result.createObjectStore(storeName, { autoIncrement: true, keyPath })
-            // 创建索引
-            if (indexs && indexs.length > 0) {
-                indexs.map((v: string) => {
-                    store.createIndex(v, v, { unique: false })
-                })
+        return new Promise((resolve, reject) => {
+            // 初始化时 顺序
+            request.onupgradeneeded = (event: any) => {
+                console.log('数据库升级成功', event)
+                const { result }: any = event.target
+                const store = result.createObjectStore(storeName, { autoIncrement: true, keyPath })
+                // 创建索引
+                if (indexs && indexs.length > 0) {
+                    indexs.map((v: string) => {
+                        store.createIndex(v, v, { unique: false })
+                    })
+                }
+                store.transaction.oncomplete = (event: any) => {
+                    console.log('创建仓库对象成功')
+                }
             }
-            store.transaction.oncomplete = (event: any) => {
-                console.log('创建仓库对象成功')
+            request.onsuccess = (event: any) => {
+                this.db = event.target.result
+                console.log('数据库打开成功', event)
+                resolve(true)
             }
-        }
-        request.onsuccess = (event: any) => {
-            this.db = event.target.result
-            console.log('数据库打开成功', event)
-        }
-        request.onerror = (event) => {
-            console.log('数据库打开失败', event)
-        }
+            request.onerror = (event) => {
+                console.log('数据库打开失败', event)
+            }
+        })
+
     }
     // 新增或修改数据库数据
     updateItem(storeName: string, data: any) {
         const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
         const request = store.put({ ...data, upDataTime: new Date().getTime() })
-        request.onsuccess = (event: any) => {
-            console.log('数据写入成功', event)
-        }
-        request.onerror = (event: any) => {
-            console.log('数据写入失败', event)
-        }
+        return new Promise((resolve, reject) => {
+            request.onsuccess = (event: any) => {
+                console.log('数据写入成功', event)
+                resolve(event.target.result)
+            }
+            request.onerror = (event: any) => {
+                console.log('数据写入失败', event)
+            }
+        })
     }
     // 删除数据库数据
     deleteItem(storeName: string, key: number | string) {
         const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
         const request = store.delete(key)
-        request.onsuccess = (event: any) => {
-            console.log('数据删除成功', event)
-        }
-        request.onerror = (event: any) => {
-            console.log('数据删除失败', event)
-        }
+        return new Promise((resolve, reject) => {
+            request.onsuccess = (event: any) => {
+                console.log('数据删除成功', event)
+                resolve(event.target.result)
+            }
+            request.onerror = (event: any) => {
+                console.log('数据删除失败', event)
+            }
+        })
     }
 
     // 查询所有数据
     getList(storeName: string) {
-        const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
+        const store = this.db.transaction(storeName).objectStore(storeName)
         const request = store.getAll()
-        request.onsuccess = (event: any) => {
-            console.log('查询所有数据成功', event.target.result)
-        }
-        request.onerror = (event: any) => {
-            console.log('查询所有数据失败', event)
-        }
+        return new Promise((resolve, reject) => {
+            request.onsuccess = (event: any) => {
+                console.log('查询所有数据成功', event.target.result)
+                resolve(event.target.result)
+            }
+            request.onerror = (event: any) => {
+                console.log('查询所有数据失败', event)
+            }
+        })
+
     }
     // 查询某一条数据
 
     getItem(storeName: string, key: number | string) {
-        const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
+        const store = this.db.transaction(storeName).objectStore(storeName)
         const request = store.get(key)
-        request.onsuccess = (event: any) => {
-            console.log('查询数据成功', event.target.result)
-        }
-        request.onerror = (event: any) => {
-            console.log('查询数据失败', event)
-        }
+        return new Promise((resolve, reject) => {
+            request.onsuccess = (event: any) => {
+                console.log('查询数据成功', event.target.result)
+                resolve(event.target.result)
+            }
+            request.onerror = (event: any) => {
+                console.log('查询数据失败', event)
+            }
+        })
     }
 }
 
--- languageMock/airbnb-ssr/src/views/index.vue
+++ languageMock/airbnb-ssr/src/views/index.vue
@@ -2,32 +2,40 @@
 import Header from "../components/header.vue";
 import Main from "../components/main.vue";
 import Footer from "../components/footer.vue";
-import IndexedDB from "../utils/indexedDB";
+// import IndexedDB from "../utils/indexedDB";
+import { fetchElephant } from "../api/index";
 
-// 数据库操作
-const airbnbDB = new IndexedDB("airbnb");
-airbnbDB.openStore("elephant", "id", ["nose", "ear"]);
-// 新增和修改
-function addDB(storeName: string) {
-  airbnbDB.updateItem(storeName, {
-    id: 1,
-    nose: "55m",
-    ear: "很大",
+// // 数据库操作
+// const airbnbDB = new IndexedDB("airbnb");
+// airbnbDB.openStore("elephant", "id", ["nose", "ear"]);
+// // 新增和修改
+// function addDB(storeName: string) {
+//   airbnbDB.updateItem(storeName, {
+//     id: 1,
+//     nose: "55m",
+//     ear: "很大",
+//   });
+// }
+// // 删除
+// function deleteDB(storeName: string, key: number | string) {
+//   airbnbDB.deleteItem(storeName, key);
+// }
+
+// // 查询
+// function getObjectStore(storeName: string) {
+//   airbnbDB.getList(storeName);
+// }
+// // 查询单条数据
+// function getObjectStoreItem(storeName: string, key: number | string) {
+//   airbnbDB.getItem(storeName, key);
+// }
+
+function getElephant() {
+  fetchElephant().then((res) => {
+    console.log(res);
   });
 }
-// 删除
-function deleteDB(storeName: string, key: number | string) {
-  airbnbDB.deleteItem(storeName, key);
-}
-
-// 查询
-function getObjectStore(storeName: string) {
-  airbnbDB.getList(storeName);
-}
-// 查询单条数据
-function getObjectStoreItem(storeName: string, key: number | string) {
-  airbnbDB.getItem(storeName, key);
-}
+getElephant();
 </script>
 
 <template>
@@ -36,10 +44,6 @@ function getObjectStoreItem(storeName: string, key: number | string) {
     <Main></Main>
     <Footer></Footer>
   </div>
-  <button @click="addDB('elephant')">增</button>
-  <button @click="deleteDB('elephant', 2)">删</button>
-  <button @click="getObjectStore('elephant')">查所有</button>
-  <button @click="getObjectStoreItem('elephant', 3)">查</button>
 </template>
 
 <style scoped lang="scss">
