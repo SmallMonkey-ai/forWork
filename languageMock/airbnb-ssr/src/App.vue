9a454a769b6579b16a918c2c0d46443a37a46017
SmallMonkey-ai <2641973190@qq.com>
2022-06-07 17:20:10 +0800

    提交本地代码（editor）

--- languageMock/airbnb-ssr/src/App.vue
+++ languageMock/airbnb-ssr/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
- 
+
 import zhCn from "element-plus/es/locale/lang/zh-cn";
 import en from "element-plus/es/locale/lang/en";
 import { ref } from "vue";
@@ -19,14 +19,13 @@ const changeLang = (language: any) => {
 
 <template>
   <el-config-provider :locale="locale">
-    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <!-- <Home msg="Hello Vue 3 + TypeScript + Vite" />
     <el-date-picker
       v-model="value1"
       type="datetime"
       :placeholder="t('message.pickDateMessage')"
     /> -->
-   
+
     <router-view></router-view>
   </el-config-provider>
 </template>
