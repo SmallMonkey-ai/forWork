95d227c8f810c1a915d7195145351a5c36153c46
SmallMonkey-ai <2641973190@qq.com>
2022-06-07 17:24:18 +0800

    wangEeitor 富文本

---
+++ vue2-wangeditor-demo/src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="app">
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column fixed prop="date" label="日期" width="150">
+      </el-table-column>
+      <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
+      <el-table-column prop="province" label="省份" width="120">
+      </el-table-column>
+      <el-table-column prop="city" label="市区" width="120"> </el-table-column>
+      <el-table-column prop="address" label="地址" width="300">
+      </el-table-column>
+      <el-table-column prop="zip" label="邮编" width="120"> </el-table-column>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small"
+            >查看</el-button
+          >
+          <el-button type="text" size="small" @click="openEditor"
+            >编辑</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-card class="box-card" v-if="editor">
+      <MyEditorWithFormula></MyEditorWithFormula>
+    </el-card>
+  </div>
+</template>
+
+<script>
+// import MyEditor from './components/MyEditor.vue'
+import MyEditorWithFormula from "./components/MyEditorWithFormula";
+// import MyEditorWithMention from './components/MyEditorWithMention'
+
+export default {
+  name: "App",
+  components: {
+    // MyEditor,
+    MyEditorWithFormula,
+    // MyEditorWithMention
+  },
+  data() {
+    return {
+      editor: false,
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1517 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1519 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+        },
+      ],
+    };
+  },
+  methods: {
+    handleClick(row) {
+      console.log(row);
+    },
+    openEditor() {
+      this.editor = true;
+    },
+  },
+};
+</script>
+
+<style>
+.box-card {
+  width: 100%;
+}
+</style>
---
+++ vue2-wangeditor-demo/src/components/MyEditorWithFormula.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <p>添加标题</p>
+    <el-input placeholder="请输入标题" v-model="input" clearable> </el-input>
+    <div>
+      <p>添加内容</p>
+    </div>
+    <div style="border: 1px solid #ccc">
+      <!-- 工具栏 -->
+      <Toolbar
+        style="border-bottom: 1px solid #ccc"
+        :editor="editor"
+        :defaultConfig="toolbarConfig"
+      />
+      <!-- 编辑器 -->
+      <Editor
+        style="height: 500px"
+        :editor="editor"
+        v-model="html"
+        :defaultConfig="editorConfig"
+        @onChange="onChange"
+        @onCreated="onCreated"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { Boot } from "@wangeditor/editor";
+import { Editor, Toolbar } from "@wangeditor/editor-for-vue";
+import formulaModule from "@wangeditor/plugin-formula";
+// console.log('formulaModule', formulaModule)
+Boot.registerModule(formulaModule);
+
+export default {
+  name: "MyEditor",
+  components: { Editor, Toolbar },
+  data() {
+    return {
+      input: "",
+      editor: null,
+      html: "<p></p>",
+      toolbarConfig: {
+        toolbarKeys: [
+          "headerSelect",
+          "|",
+          "bold",
+          "underline",
+          "italic",
+          "color",
+          "bgColor",
+          "|",
+          "fontSize",
+          "fontFamily",
+          "lineHeight",
+          "|",
+          "bulletedList",
+          "numberedList",
+          "todo",
+          "|",
+          "emotion",
+          "insertLink",
+          "insertTable",
+          "codeBlock",
+          "divider",
+          "|",
+          "uploadImage",
+          "insertImage",
+          "|",
+          "fullScreen",
+        ],
+        excludeKeys: [
+          /* 隐藏哪些菜单 */
+        ],
+        insertKeys: {
+          index: 0,
+          keys: [
+            "insertFormula", // “插入公式”菜单
+            // 'editFormula' // “编辑公式”菜单
+          ],
+        },
+      },
+      editorConfig: {
+        placeholder: "请输入内容...",
+        // autoFocus: false,
+
+        // 所有的菜单配置，都要在 MENU_CONF 属性下
+        MENU_CONF: {},
+
+        hoverbarKeys: {
+          formula: {
+            menuKeys: ["editFormula"], // “编辑公式”菜单
+          },
+        },
+      },
+    };
+  },
+  methods: {
+    onCreated(editor) {
+      this.editor = Object.seal(editor); // 【注意】一定要用 Object.seal() 否则会报错
+      console.log(editor.getAllMenuKeys());
+    },
+    onChange(editor) {
+      console.log("onChange", editor.getHtml()); // onChange 时获取编辑器最新内容
+    },
+    getEditorText() {
+      const editor = this.editor;
+      if (editor == null) return;
+      console.log(editor.getText()); // 执行 editor API
+    },
+  },
+  beforeDestroy() {
+    const editor = this.editor;
+    if (editor == null) return;
+    editor.destroy(); // 组件销毁时，及时销毁 editor ，重要！！！
+  },
+};
+</script>
+
+<style src="@wangeditor/editor/dist/css/style.css"></style>
